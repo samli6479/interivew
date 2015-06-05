d3792b23fcee1e61b2b3ffcc3c850aad94f6c0ae
pritesh <pritesh@gowatchit.com>
2015-06-05 16:32:20 -0400

    removed everything but movies.

--- src/main/java/gowatchit/com/matcher/IMatcher.java
+++ src/main/java/gowatchit/com/matcher/IMatcher.java
@@ -5,6 +5,7 @@ import gowatchit.com.matcher.dto.MatchedPair;
 import java.util.List;
 
 public interface IMatcher {
+
   List<MatchedPair> process(String matchFromFile, String moviesDb, String contributorsDb);
 
 }
