44768df93d1bbaf01ca7e55dbe3a2f2906302add
dmitry <dmitry.zutikov@gmail.com>
2015-06-24 22:17:34 -0400

    fixed data file paths in spec

--- ruby/spec/lib/xbox_matcher_spec.rb
+++ ruby/spec/lib/xbox_matcher_spec.rb
@@ -4,7 +4,7 @@ describe 'XboxMatcher' do
 
   it 'matches xbox movies to gowatchit movies' do
     matcher = XboxMatcher.new
-    matched_movies = matcher.process('data/xbox_feed.csv', 'movies.csv', 'actors_and_directors.csv')
+    matched_movies = matcher.process('data/xbox_feed.csv', 'data/movies.csv', 'data/actors_and_directors.csv')
 
     expect(matched_movies).to_not be_empty
   end
